2fd7e01bc2765a461399446066ba04a01ac89226
drewgaze <adrmcgarrity@gmail.com>
2015-12-21 17:11:31 -0500

    jwt and bcrypt for auth

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var express = require('express');
 var jwt = require('jwt-simple');
 var _ = require('lodash');
 var bcrypt = require('bcrypt');
+var User = require('./user');
+var secret = require('./secret');
 
 var app = express();
 
 app.use(require('body-parser').json());
 
-var users = [{username: 'user', password: '$2a$10$VtTT4D/oMAnYyrHwFyx8hOBkeutbIXM3gC/k4eATBV9rzrQgIschu'}];
-var secretKey = 'mySecret';
+var secretKey = secret;
 
 function findUserByUsername(username) {
 
@@ -22,24 +23,58 @@ function validateUser(user, password, callback) {
 
 app.post('/session', function(req, res) {
 
-	var user = findUserByUsername(req.body.username);
+	var username = req.body.username;
 
-	validateUser(user, req.body.password, function(err, valid) {
+	User.findOne({username: username})
+	.select('password')
+	.exec(function(err, user) {
 
-		if (err || !valid) return res.status(401);
+		if (err) return next(err);
+		if (!user) return res.sendStatus(401);
 
-		var token = jwt.encode({username: user.username}, secretKey);
+		bcrypt.compare(req.body.password, user.password, function(err, valid) {
 
-		res.json(token);
+			if (err) return next(err);
+			if (!valid) return res.sendStatus(401);
+
+			var token = jwt.encode({username: username}, secretKey);
+			res.json(token);
+		});
 	});
 });
 
 app.get('/user', function(req, res) {
 
 	var token = req.headers['x-auth'];
-	var user = jwt.decode(token, secretKey);
-	//TODO: get user from db
-	res.json(user);
+
+	console.log(token);
+
+	var decoded = jwt.decode(token, secretKey);
+
+	console.log(decoded);
+	
+	User.findOne({username: decoded.username}, function(err, user) {
+
+		if (err) console.log(err);
+
+		res.json(user);
+	});
+});
+
+app.post('/user', function(req, res, next) {
+
+	var user = new User({username: req.body.username});
+	bcrypt.hash(req.body.password, 10, function(err, hash) {
+
+		user.password = hash;
+
+		user.save(function(err, user) {
+
+			if (err) throw next(err);
+
+			res.sendStatus(201);
+		});
+	});
 });
 
 app.listen(3000);
